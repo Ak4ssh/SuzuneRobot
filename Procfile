886937148d4d1f2348e25c521edf9211bce7d807
desinobita <84653553+desinobita@users.noreply.github.com>
2021-10-12 18:00:47 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m SuzuneBot
+web: python3 -m SuzuneBot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE SuzuneBot
+:: Enables virtual env mode and then starts SuzuneBot
+env\scripts\activate.bat && py -m SuzuneBot
