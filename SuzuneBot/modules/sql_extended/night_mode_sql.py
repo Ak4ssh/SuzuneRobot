d63459ef78a56d5cd09ee57d32a125d572c41ec9
desinobita <84653553+desinobita@users.noreply.github.com>
2021-11-25 01:08:08 +0530

    Update night_mode_sql.py

--- SuzuneBot/modules/sql_extended/night_mode_sql.py
+++ SuzuneBot/modules/sql_extended/night_mode_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from suzunebot.modules.sql import BASE, SESSION
+from SuzuneBot.modules.sql import BASE, SESSION
 
 
 class Nightmode(BASE):
